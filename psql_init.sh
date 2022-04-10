95eef7759ddeebe7bcd65c26ec66d633e24e40e1
envops-git <github.envops@gmail.com>
2022-04-10 14:54:58 +0300

    fix postgres role creation in psql_init.sh

--- psql_init.sh
+++ psql_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-`psql -c echo " CREATE ROLE postgres WITH SUPERUSER CREATEDB CREATEROLE LOGIN ENCRYPTED PASSWORD 'postgres';"` 
+psql -c echo " CREATE ROLE postgres WITH SUPERUSER CREATEDB CREATEROLE LOGIN ENCRYPTED PASSWORD 'postgres';" 
 
 psql -f pg_backup.bak kong
