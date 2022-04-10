5110fa4c2a5b75a0c56bae4eff839b781dbeafff
envops-git <github.envops@gmail.com>
2022-04-10 15:25:55 +0300

    fix postgres role creation in psql_init.sh

--- psql_init.sh
+++ psql_init.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -e
 
-export PGUSER=postgres
-psql <<- EOSQL
-    CREATE USER postgres;
-    GRANT ALL PRIVILEGES ON TO docker;
-EOSQL
-
-psql -f pg_backup.bak postgres
+psql -f pg_backup.bak kong
